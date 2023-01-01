91232baaa4a64cfce9ac477f25116b5e8159f9b4
nurj0hn <nurj0hn>
2023-01-01 17:28:59 +0600

    modified jwt provider class and add security config why i dont know i just follow tutor but i have a problem with it some lib in not supported i understand so im stuck :)

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.security;
+
+import com.example.demo.services.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        proxyTargetClass = true
+)
+public class SecurityConfig {
+
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+
+
+
+}
